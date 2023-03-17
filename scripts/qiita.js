a7c6119c851d03ab5d9ced95b6233e211cf5c8c0
soburi <591959ant@gmail.com>
2023-03-17 15:13:25 +0900

    comment

--- scripts/qiita.js
+++ scripts/qiita.js
@@ -1,33 +1,48 @@
-const userId = 'soburi06'; // 自分のQiitaユーザー名
-const url = `https://qiita.com/api/v2/users/${userId}/items`; // APIエンドポイント
-const container = document.getElementById('qiita-posts'); // 投稿一覧を表示する要素のID
+// Qiitaユーザー名を定数として定義
+const userId = 'soburi06';
 
+// APIエンドポイントを定数として定義（${}内には変数userIdが入ります）
+const url = `https://qiita.com/api/v2/users/${userId}/items`;
+
+// 投稿一覧を表示する要素のIDを取得して、container定数に代入する
+const container = document.getElementById('qiita-posts');
+
+// fetch関数によってQiita APIからデータを取得
 fetch(url, {
   headers: {
-    'Authorization': 'Bearer 2818a0595b34040ffe4d93988b435a68df2bb897' // アクセストークンを設定
+    // Authorizationヘッダーにアクセストークンを指定
+    'Authorization': 'Bearer 2818a0595b34040ffe4d93988b435a68df2bb897'
   }
 })
+  // 読み込んだレスポンスの内容をJSON形式に変換してリターンする
   .then(response => response.json())
+  // JSON形式のデータを引数にしてコールバック関数を実行
   .then(data => {
-    // 取得した投稿データをループしてHTMLを作成
+    // HTMLの文字列を作成するための変数htmlを定義
     let html = '';
+    // forEachメソッドで投稿データ一つずつループ処理を行う
     data.forEach(post => {
+      // テンプレートリテラルを使ってHTMLの文字列を作成
       html += `
               <div class="qiita-post">
                 <a class="qiita-link" href="${post.url}" target="_blank" rel="noopener noreferrer"></a>
                 <p>${post.title}</p>
                 <div class="tags">
             `;
+      
+      // 投稿のタグ情報をループ処理でタグ名をHTML書式に追加する
       post.tags.forEach(function(tag) {
         html+= `<a class="tag-name" href="https://qiita.com/tags/${tag.name}">`+ tag.name + '</a>';
       });
-      html+=`   </div>
+      
+      html += `   </div>
                 <div class="date">${post.created_at.slice(0, 10)}</div>
               </div>
             `;
     });
-
-    // HTMLを要素に表示
+    
+    // 完成したHTML文字列を要素に挿入
     container.innerHTML = html;
   })
+  // エラー時のコールバック関数
   .catch(error => console.error(error));
