9b137cf2c89851f38feec99ca89dbe827fba8a3e
soburi <591959ant@gmail.com>
2023-03-20 02:33:34 +0900

    コンテンツを動的に変更

--- scripts/insert.js
+++ scripts/insert.js
@@ -1,12 +1,16 @@
-const mainContent = document.getElementById('main-content');
-const url = `contents/test.html`;
 
-fetch(url)
-    .then((response) => {
-        if(!response.ok){
-            throw new Error(`HTTP error:${response.status}`);
-        }
-        return response.text();
-    })
-    .then((html) => mainContent.innerHTML = html)
-    .catch((error) => mainContent.innerHTML =  `<p>Could not fetch verse: ${error}</p>`);
+let url = `contents/test.html`;
+
+function loadContent(url){
+    const mainContent = document.getElementById('main-content');
+    fetch(url)
+        .then((response) => {
+            if(!response.ok){
+                throw new Error(`HTTP error:${response.status}`);
+            }
+            return response.text();
+        })
+        .then((html) => mainContent.innerHTML = html)
+        .catch((error) => mainContent.innerHTML =  `<p>Could not fetch verse: ${error}</p>`);
+}
+
