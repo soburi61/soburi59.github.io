868e9f508eb1d6bd25d524abe07a9fb2c951dc2b
soburi59 <591959ant@gmail.com>
2023-05-11 11:52:41 +0900

    update logo

--- scripts/logoChange.js
+++ scripts/logoChange.js
@@ -4,16 +4,16 @@ let myImage = document.querySelector('.logo');
 myImage.addEventListener('mouseover',function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/logo.png') {
-      myImage.setAttribute('src','images/logo-hover.png');
+      myImage.setAttribute('src','../images/logo-hover.png');
     } else {
-      myImage.setAttribute('src','images/logo.png');
+      myImage.setAttribute('src','../images/logo.png');
     }
 });
 myImage.addEventListener('mouseleave',function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/logo.png') {
-      myImage.setAttribute('src','images/logo-hover.png');
+      myImage.setAttribute('src','../images/logo-hover.png');
     } else {
-      myImage.setAttribute('src','images/logo.png');
+      myImage.setAttribute('src','../images/logo.png');
     }
 });
